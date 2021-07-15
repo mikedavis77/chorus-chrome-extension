d4ea29717c8356c5267bb22c8410c02de2f4f1e6
Mike Davis <mikedavisis@gmail.com>
2021-07-15 09:28:18 +0100

    Updated settings to be in popup rather than options page

--- popup.js
+++ popup.js
@@ -1,11 +1,46 @@
-// Initialize button with user's preferred color
-//let addChorusLink = document.getElementById('addChorusLink');
+const viewChorusEl = document.querySelector('#viewChorusId');
+const editChorusEl = document.querySelector('#editChorusId');
+
 const chorusUrl = document.querySelector('#chorusURL');
+const chorusUrlInput = document.querySelector('#chorusInput');
+let myChorusId = '';
 
 chrome.storage.sync.get('chorusId', ({ chorusId }) => {
-  chorusUrl.innerHTML = chorusId;
+  console.log('chorusId', chorusId);
+  if (chorusId !== '') {
+    console.log('show the current link');
+    viewChorusEl.classList.remove('hideme');
+    editChorusEl.classList.add('hideme');
+    setChorusIdDisplayValue(chorusId);
+  } else {
+    console.log('no current link');
+    viewChorusEl.classList.add('hideme');
+    editChorusEl.classList.remove('hideme');
+  }
 });
 
-document.querySelector('#go-to-options').addEventListener('click', function () {
-  window.open(chrome.runtime.getURL('options.html'));
+document.querySelector('#editChorusBtn').addEventListener('click', () => {
+  viewChorusEl.classList.add('hideme');
+  editChorusEl.classList.remove('hideme');
+  chorusUrlInput.value = myChorusId;
 });
+
+document.querySelector('#saveChorusBtn').addEventListener('click', () => {
+  const chorusId = chorusUrlInput.value;
+  console.log('set id', chorusId);
+  chrome.storage.sync.set({ chorusId });
+  setChorusIdDisplayValue(chorusId);
+
+  viewChorusEl.classList.remove('hideme');
+  editChorusEl.classList.add('hideme');
+});
+
+document.querySelector('#cancelChorusBtn').addEventListener('click', () => {
+  viewChorusEl.classList.remove('hideme');
+  editChorusEl.classList.add('hideme');
+});
+
+function setChorusIdDisplayValue(chorusId) {
+  myChorusId = chorusId;
+  chorusUrl.innerHTML = chorusId;
+}
