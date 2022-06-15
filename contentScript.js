1295a4a6591c82ec89065293a20fb633859b516c
Mike Davis <mikedavisis@gmail.com>
2022-06-15 09:08:33 +0100

    Refactored main script

--- contentScript.js
+++ contentScript.js
@@ -1,98 +1,51 @@
-function addChorusButtonToEvent(callCount = 0) {
-  console.log('addChorusButtonToEvent', callCount);
-  if (/calendar.google.com.*\/eventedit[^/]*/i.test(window.location.toString())) {
-    console.log('found event page');
-    const zoomBtnContainer = document.querySelector('.zoom-video-sec');
-    // DOM loading might be delayed, so retry 5 times with some backoff.
-    if (callCount < 5 && zoomBtnContainer === null) {
-      window.setTimeout(function () {
-        addChorusButtonToEvent(callCount + 1);
-      }, 500);
-      return;
-    }
-
-    if (zoomBtnContainer !== null) {
-      const chorusBtnContainer = document.querySelector('.chorus-video-sec');
-      if (chorusBtnContainer !== null) {
-        // console.log('already have chrous button');
-        return;
-      }
-
-      const locationEl = document.querySelector('[aria-label="Location"]');
-      const hasChorusMeetingUrl = locationEl.value !== '' && locationEl.value.indexOf('go.chorus.ai') > -1;
-      const chorusMeetingUrl = hasChorusMeetingUrl ? locationEl.value : null;
-
-      // Create add chorus button. - repeated
-      const addBtn = document.createElement('button');
-      addBtn.id = 'chorus-schedule_button';
-      addBtn.className = 'btn-meeting';
-      addBtn.innerHTML = 'Make it a Chorus Meeting';
-      const addBtnWrapper = document.createElement('div');
-      addBtnWrapper.id = 'chorus-schedule_button-wrapper';
-      addBtnWrapper.appendChild(addBtn);
-
-      // Create join chorus link. - repeated
-      const joinBtnWrapper = document.createElement('div');
-      joinBtnWrapper.style = 'display:flex';
-      const joinLink = document.createElement('a');
-      joinLink.id = 'chorus_schedule_meeting_url';
-      joinLink.className = 'btn-meeting hideme';
-      joinLink.target = '_blank';
-      joinLink.href = chorusMeetingUrl;
-      joinLink.style = 'display:inline';
-      joinLink.innerHTML = 'Join Chorus Meeting';
-      const joinLinkRemoveIcon = document.createElement('i');
-      joinLinkRemoveIcon.className = 'icon-trash';
-      joinLinkRemoveIcon.title = 'Remove Chorus Meeting';
-      joinBtnWrapper.appendChild(joinLink);
-      joinBtnWrapper.appendChild(joinLinkRemoveIcon);
-
-      // Replace chorus link with relevent button.
-      const icon = document.createElement('span');
-      icon.className = 'DPvwYc chorusLogo';
-      const iconWrapper = document.createElement('div');
-      iconWrapper.className = 'tzcF6';
-      iconWrapper.appendChild(icon);
-
-      // btn wrapper - repeated
-      const btnWrapper = document.createElement('div');
-      btnWrapper.className = 'j3nyw PxPKzc chorus-btn';
-      btnWrapper.appendChild(hasChorusMeetingUrl ? joinBtnWrapper : addBtnWrapper);
-
-      const mainWrapper = document.createElement('div');
-      mainWrapper.id = 'chorus-video-sec';
-      mainWrapper.className = 'FrSOzf chorus-video-sec';
-      mainWrapper.appendChild(iconWrapper);
-      mainWrapper.appendChild(btnWrapper);
-
-      zoomBtnContainer.insertAdjacentElement('afterEnd', mainWrapper);
-
-      // event listeners repeated
-      const chorusBtn = document.querySelector('#chorus-schedule_button');
-      if (chorusBtn !== null) {
-        chorusBtn.addEventListener('click', () => {
-          chrome.storage.sync.get('chorusId', ({ chorusId }) => {
-            const chorusBasedomin = 'https://go.chorus.ai/';
-            setCalendarLocation(locationEl, `${chorusBasedomin}${chorusId}`);
-          });
-        });
-      }
-
-      const chorusRemoveBtn = document.querySelector('.chorus-btn i');
-      if (chorusRemoveBtn !== null) {
-        chorusRemoveBtn.addEventListener('click', () => {
-          setCalendarLocation(locationEl, '');
-        });
-      }
-    } else {
-      console.log("No 'Make it a Zoom meeting' button found on the page");
-    }
-  } else {
-    console.log('no event match');
+const addChorusButtonToEvent = (callCount = 0) => {
+  if (!/calendar.google.com.*\/eventedit[^/]*/i.test(window.location.toString())) {
+    return;
   }
-}
 
-function setCalendarLocation(locationEl, text) {
+  const zoomBtnContainer = document.querySelector('.zoom-video-sec');
+  // DOM loading might be delayed, so retry 10 times with some backoff.
+  if (callCount < 10 && zoomBtnContainer === null) {
+    window.setTimeout(function () {
+      addChorusButtonToEvent(callCount + 1);
+    }, 200);
+    return;
+  }
+
+  if (zoomBtnContainer === null) {
+    console.error("No 'Make it a Zoom meeting' button found on the page");
+  }
+
+  const chorusBtnContainer = document.querySelector('.chorus-video-sec');
+  if (chorusBtnContainer !== null) {
+    return;
+  }
+
+  // Create chorus icon.
+  const icon = document.createElement('span');
+  icon.className = 'DPvwYc chorusLogo';
+  const iconWrapper = document.createElement('div');
+  iconWrapper.className = 'tzcF6';
+  iconWrapper.appendChild(icon);
+
+  // Set the chorus button link.
+  const chorusBtn = createChorusButton();
+
+  // Create chorus wrapper.
+  const chorusBtnWrapper = document.createElement('div');
+  chorusBtnWrapper.id = 'chorus-video-sec';
+  chorusBtnWrapper.className = 'FrSOzf chorus-video-sec';
+  chorusBtnWrapper.appendChild(iconWrapper);
+  chorusBtnWrapper.appendChild(chorusBtn);
+
+  zoomBtnContainer.insertAdjacentElement('afterEnd', chorusBtnWrapper);
+
+  // Add event listeners.
+  setEventListeners();
+};
+
+const setCalendarLocation = (text) => {
+  const locationEl = getLocationElement();
   locationEl.dispatchEvent(new MouseEvent('click', { bubbles: true }));
   locationEl.focus();
   for (const type of ['keydown', 'keypress', 'keyup']) {
@@ -112,11 +65,19 @@ function setCalendarLocation(locationEl, text) {
   locationEl.dispatchEvent(new MouseEvent('input', { bubbles: true }));
   locationEl.blur();
 
-  const chorusBtnContainer = document.querySelector('.chorus-btn');
-  const hasChorusMeetingUrl = locationEl.value !== '' && locationEl.value.indexOf('go.chorus.ai') > -1;
-  const chorusMeetingUrl = hasChorusMeetingUrl ? locationEl.value : null;
+  const chorusBtnWrapper = document.querySelector('.chorus-btn');
 
-  // Create add chorus button.
+  // Replace chorus button with action - create/ join.
+  const chorusBtn = createChorusButton();
+  chorusBtnWrapper.replaceWith(chorusBtn);
+
+  // Add event listeners.
+  setEventListeners();
+};
+
+const getLocationElement = () => document.querySelector('[aria-label="Location"]');
+
+const createAddChorusButton = () => {
   const addBtn = document.createElement('button');
   addBtn.id = 'chorus-schedule_button';
   addBtn.className = 'btn-meeting';
@@ -124,8 +85,10 @@ function setCalendarLocation(locationEl, text) {
   const addBtnWrapper = document.createElement('div');
   addBtnWrapper.id = 'chorus-schedule_button-wrapper';
   addBtnWrapper.appendChild(addBtn);
+  return addBtnWrapper;
+};
 
-  // Create join chorus link.
+const createJoinChorusButton = (chorusMeetingUrl) => {
   const joinBtnWrapper = document.createElement('div');
   joinBtnWrapper.style = 'display:flex';
   const joinLink = document.createElement('a');
@@ -140,20 +103,31 @@ function setCalendarLocation(locationEl, text) {
   joinLinkRemoveIcon.title = 'Remove Chorus Meeting';
   joinBtnWrapper.appendChild(joinLink);
   joinBtnWrapper.appendChild(joinLinkRemoveIcon);
+  return joinBtnWrapper;
+};
 
-  // Replace chorus link with relevent button.
+const createChorusButton = () => {
+  const locationEl = getLocationElement();
+  const hasChorusMeetingUrl = locationEl.value !== '' && locationEl.value.indexOf('go.chorus.ai') > -1;
+  const chorusMeetingUrl = hasChorusMeetingUrl ? locationEl.value : null;
+
+  // Create add chorus button.
+  const addBtnWrapper = createAddChorusButton();
+  // Create join chorus link.
+  const joinBtnWrapper = createJoinChorusButton(chorusMeetingUrl);
+
   const btnWrapper = document.createElement('div');
   btnWrapper.className = 'j3nyw PxPKzc chorus-btn';
   btnWrapper.appendChild(hasChorusMeetingUrl ? joinBtnWrapper : addBtnWrapper);
-  chorusBtnContainer.replaceWith(btnWrapper);
+  return btnWrapper;
+};
 
-  // add button event listeners.
+const setEventListeners = () => {
   const chorusBtn = document.querySelector('#chorus-schedule_button');
   if (chorusBtn !== null) {
     chorusBtn.addEventListener('click', () => {
       chrome.storage.sync.get('chorusId', ({ chorusId }) => {
-        const chorusBasedomin = 'https://go.chorus.ai/';
-        setCalendarLocation(locationEl, `${chorusBasedomin}${chorusId}`);
+        setCalendarLocation(`https://go.chorus.ai/${chorusId}`);
       });
     });
   }
@@ -161,16 +135,13 @@ function setCalendarLocation(locationEl, text) {
   const chorusRemoveBtn = document.querySelector('.chorus-btn i');
   if (chorusRemoveBtn !== null) {
     chorusRemoveBtn.addEventListener('click', () => {
-      setCalendarLocation(locationEl, '');
+      setCalendarLocation('');
     });
   }
-}
+};
 
 // @todo add button to popup window when adding/ editing event
-//addChorusButtonToEvent(0);
-// console.log('contnetScript loaded');
 chrome.runtime.onMessage.addListener(function (request) {
-  // console.log('onmessage listener', request);
   if (request.execute_addChorusButtonToEvent == true) {
     addChorusButtonToEvent(0);
   }
